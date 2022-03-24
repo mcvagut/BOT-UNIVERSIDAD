2941ab9207462ea048e56057e8ef182767984f86
mcvagut <mcvagut@gmail.com>
2022-03-24 19:17:25 -0400

    Proyecto de Bot Universitario

---
+++ universidad/src/test/java/bo/edu/ucb/ingsoft/universidad/UniversidadApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.universidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
